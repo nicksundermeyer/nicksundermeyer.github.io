14eedc2cfb607050f2b8ef5fd811c4be848b0933
Nicholas Sundermeyer <nicsunde@cs.du.edu>
2018-12-18 20:11:21 -0800

    Starting new react-based version

--- README.md
+++
@@ -1,1 +0,0 @@
-Portfolio website created by me using HTML, CSS, and JS. Using templates from https://startbootstrap.com/
--- scripts/main.js
+++
@@ -1,7 +0,0 @@
-// smooth scroll to sections
-$(".nav-link").click(function() {
-    var section = $(this).attr("href");
-    $("html, body").animate({
-        scrollTop: $(section).offset().top - 50
-    });
-});
