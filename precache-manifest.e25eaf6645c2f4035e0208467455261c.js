1fd36daffdc09d1468eaa635af261f7a2864671e
Nicholas Sundermeyer <nicsunde@cs.du.edu>
2019-07-24 15:12:46 -0600

    Updated

---
+++ precache-manifest.e25eaf6645c2f4035e0208467455261c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c711844b9860dbe87010",
+    "url": "/static/js/main.f8dbfb69.chunk.js"
+  },
+  {
+    "revision": "6cc06b162bd4c81490f8",
+    "url": "/static/js/2.e4098da8.chunk.js"
+  },
+  {
+    "revision": "c711844b9860dbe87010",
+    "url": "/static/css/main.cd1de956.chunk.css"
+  },
+  {
+    "revision": "a8c8995cac7f8fd4a99a6b24f93899d1",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.965aa0ee440088c289bb0444db81f838.js"
+  "/precache-manifest.e25eaf6645c2f4035e0208467455261c.js"
 );
 
 workbox.clientsClaim();
