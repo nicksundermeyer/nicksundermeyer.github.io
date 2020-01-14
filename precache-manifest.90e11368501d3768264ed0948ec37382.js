ad96cbc4965fa5cc51850128e3a2298724260ce9
Nick Sundermeyer <nicksundermeyer@gmail.com>
2020-01-14 09:34:59 -0700

    adding videos

---
+++ precache-manifest.90e11368501d3768264ed0948ec37382.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbfeca9f386cf4405423e69b31539c1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b29b74a2cee5c9405e44",
+    "url": "/static/css/main.297ddd71.chunk.css"
+  },
+  {
+    "revision": "f08fd9c2234d066a757e",
+    "url": "/static/js/2.99a19bca.chunk.js"
+  },
+  {
+    "revision": "3c010806e66d64852a42ca3f64c71297",
+    "url": "/static/js/2.99a19bca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b29b74a2cee5c9405e44",
+    "url": "/static/js/main.9833b146.chunk.js"
+  },
+  {
+    "revision": "48e6ed1aa7d2e9247982",
+    "url": "/static/js/runtime-main.bc1e2fa9.js"
+  }
+]);
